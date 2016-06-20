f82c095bc035149f935d37b423d8a6dfbc2041b7
Abed Fayyad <AbedFayyad@live.com>
2016-06-20 01:55:33 -0400

    Initial Commit

---
+++ Tinker2D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tinker2D
+//
+//  Created by Abed on 2016-06-20.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
